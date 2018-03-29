2a81c2c4c2cf3fceeeaf03a843eda178cb8a9358
Sebastian Wallace <sebjwallace@gmail.com>
2018-03-29 22:22:37 +0100

    scratching in SDRDictionary and SDRClassifier

---
+++ src/core/SDRClassifier.py
@@ -0,0 +1,7 @@
+class SDRClassifier:
+
+    """
+    A wrapper around SDRMap. If an key does not retrieve a value it will
+    map the key with a new SDR value. SDRClassifiers can be combined into
+    a hierarchy for progressive feature detection.
+    """
---
+++ src/core/SDRDictionary.py
@@ -0,0 +1,6 @@
+class SDRDictionary:
+
+    """
+    Associate an SDR to any value, i.e object, list, string, number, etc.
+    anything that describes the SDR
+    """
